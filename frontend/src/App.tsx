0ed2483ad8b1e01952ded8b3653145c700ac9680
gabi <z5592426@ad.unsw.edu.au>
2026-04-09 12:23:48 +1000

    began to implement shell for register and login, following along the lectures

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Routes, Route, Link } from 'react-router-dom';
+
 import './App.css'
+import Register from './page/Register';
 
 function App() {
-  const [count, setCount] = useState(0);
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      NavBar
+      <br></br>
+      <Link to="/">Home</Link>
+      <br></br>
+      <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
+    </div>
+
+
+      <Routes>
+        <Route path="/" element={<p>Homepage!</p>} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<p>Login!</p>} />
+      </Routes>
     </>
   )
 }
+
 
 export default App
