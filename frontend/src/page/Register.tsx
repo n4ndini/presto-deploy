32ccb9f6dbcc1e626cb14e0de21dc6462442f732
gabi <z5592426@ad.unsw.edu.au>
2026-04-10 09:35:07 +1000

    implemented password confirmation error checking

--- frontend/src/page/Register.tsx
+++ frontend/src/page/Register.tsx
@@ -1,11 +1,48 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 function Register() {
-  
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [error, setError] = useState(''); // implement error messages
+
+//   npm install axios 
+// check local port is right
+  const register = async () => {
+
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+   
+    const response = await axios.post("http://localhost:5005/admin/auth/register", {
+      name,
+      email,
+      password,
+    });
+    const token = response.data.token;
+    localStorage.setItem('token', token);
+
+    // go to dashboard
+    
+  };
+
   return (
     <>
-        Register
+        {/* use component libraries?? */}
+        Register<br />
+        Name: <input type="text" value={name} onChange={e => setName(e.target.value)} /><br />
+        Email: <input type="text" value={email} onChange={e => setEmail(e.target.value)} /><br />
+        Password: <input type="text" value={password} onChange={e => setPassword(e.target.value)} /><br />
+        Confirm Password: <input type="text" value={confirmPassword} onChange={e => setConfirmPassword(e.target.value)} /><br />
+
+        <button type="button" onClick={register}>Register</button>
     </>
   )
 }
-
 
 export default Register
