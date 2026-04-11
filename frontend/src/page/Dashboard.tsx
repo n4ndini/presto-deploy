c11d1abd31c12c7db2d7d55e7c9c55dada0d3d3f
gabi <z5592426@ad.unsw.edu.au>
2026-04-11 10:56:09 +1000

    planning for section and chose types for major components

--- frontend/src/page/Dashboard.tsx
+++ frontend/src/page/Dashboard.tsx
@@ -1,15 +1,43 @@
+import axios from "axios";
 import { useState } from "react";
+
+type Slide = Record<string, string>; // before and after? or content and next? tbd!
+
+type Presentation = {
+  id: number,
+  name: string,
+  desc: string,
+  thumbnail: string,
+  slides: Slide[];
+}
+
+type Store = {
+  presentations?: Presentation[];
+}
 
 function Dashboard() {
   const [showCreatePres, setShowCreatePres] = useState(false);
   const [name, setName] = useState('');
   const [desc, setDesc] = useState('');
   const [thumbnail, setThumbnail] = useState('');
-
+  const [error, setError] = useState('');
   const token = localStorage.getItem('token');
 
+
   const createPresentation = async () => {
-    
+    // error checking here
+
+
+
+    // ds structure --> can have an arr of Presentations w
+    // id, name, desc, thumbnail, slides [{}] (arr of objects)
+
+    // have to GET curr store, then return og store + new presentation
+    try {
+      await axios.post('http//localhost:5005/')
+    } catch {
+
+    }
   }
 
   return (
