4219347fa5dde496402ee27eefc0f9a73f121e60
gabi <z5592426@ad.unsw.edu.au>
2026-04-13 13:57:05 +1000

    added useEffect to fetch presentation and then input first slide

--- frontend/src/page/Presentation.tsx
+++ frontend/src/page/Presentation.tsx
@@ -8,11 +8,30 @@ function Presentation() {
   const { id } = useParams();
   const token = localStorage.getItem('token');
   const [presentation, setPresentation] = useState<PresentationType | null>(null);
-  const [currSlide, setCurrSlide] = useState(0);
+  // const [currSlide, setCurrSlide] = useState(0);
   const [showDeletePopup, setShowDeletePopup] = useState(false);
   const navigate = useNavigate();
 
   // NEED TO FETCH PRESENTATION, LOAD OG SLIDE AND DEAL W PRESENTATION ERR
+  useEffect(() => {
+    const fetchPresentation = async () => {
+      const res = await axios.get('http://localhost:5005/store', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      const store = res.data.store;
+      const found = store.presentations.find((p: PresentationType) => p.id === Number(id));
+
+      setPresentation (found || null);
+    };
+    fetchPresentation();
+  }, [id]);
+
+  if (!presentation) {
+    return <p>Loading...</p>;
+  }
 
   const deletePresentation = async () => {
     const res = await axios.get('http://localhost:5005/store', {
@@ -36,10 +55,12 @@ function Presentation() {
     navigate("/dashboard");
   };
 
+  const firstSlide = presentation.slides[0];
+
   return (
     <>
       {/* <h1>Presentation {presentation.name}</h1> */}
-      <h1>Presentation {id}</h1>
+      <h1>Presentation {id}: {presentation.name}</h1>
 
       <button onClick={() => navigate('/dashboard')}>Back</button>
       <button onClick={() => setShowDeletePopup(true)}>Delete Presentation</button>
@@ -54,8 +75,10 @@ function Presentation() {
             <button onClick={() => setShowDeletePopup(false)}>No</button>
         </div>
       )}
+
+      <h2>Slide: {firstSlide.id}</h2>
+      <p>{firstSlide.content || "(empty slide)"}</p>
     </>
-    
   )
 }
 
