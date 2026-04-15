ad866e21ab7ef2f7a006fe8e8363df7ed51a0e05
gabi <z5592426@ad.unsw.edu.au>
2026-04-15 13:11:45 +1000

    added behaviour and appearance controls in separate TextElements.tsx page

---
+++ frontend/src/page/elems/TextElement.tsx
@@ -0,0 +1,41 @@
+type Props = {
+  elem: any;
+  onDelete: (id: number) => void;
+};
+
+// handles behaviour and appearance of Text Element
+// renders text box, handles right click delete, double click edit and styling
+function TextElement({ elem, onDelete }: Props) {
+  return (
+    <div
+      onContextMenu={(e) => {
+        e.preventDefault();
+        onDelete(elem.id);
+      }}
+      onDoubleClick={() => {
+        console.log("edit later");
+      }}
+      style={{
+        position: "absolute",
+        left: `${elem.x}%`,
+        top: `${elem.y}%`,
+        width: `${elem.width}%`,
+        height: `${elem.height}%`,
+
+        border: "1px solid lightgrey",
+        overflow: "auto",
+        whiteSpace: "pre",
+        textAlign: "left",
+
+        fontSize: `${elem.fontSize}em`,
+        color: elem.colour,
+
+        padding: "4px",
+      }}
+    >
+      {elem.content}
+    </div>
+  );
+}
+
+export default TextElement;
