11d54bc0917fd041b5fecc2749cc4992a6fb825e
z5593045 <z5593045@ad.unsw.edu.au>
2026-04-20 00:57:41 +1000

    FS4: added imports

--- frontend/src/page/PresentationPreview.tsx
+++ frontend/src/page/PresentationPreview.tsx
@@ -1,3 +1,10 @@
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import type { ElementType, PresentationType, SlideType } from "../types";
+import { getPresentationById } from "./Helpers";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 function PresentationPreview() {
 
 
