8877b9a7e9d5e4a77a75a1b9949f8ec547911209
gabi <z5592426@ad.unsw.edu.au>
2026-04-20 12:58:31 +1000

    added restore fn that restores a version history and fixed spacing so its 2 instead of 4 spaces

--- frontend/src/page/HistoryPage.tsx
+++ frontend/src/page/HistoryPage.tsx
@@ -2,21 +2,37 @@ import { useParams } from "next/navigation";
 import { useNavigate } from "react-router-dom";
 import type { HistoryEntry } from "../types";
 import { useEffect, useState } from "react";
+import { getPresentationById, updatePresentation } from "./Helpers";
 
 function HistoryPage() {
-    const { id } = useParams();
-    const token = localStorage.getItem('token');
-    const navigate = useNavigate();
+  const { id } = useParams();
+  const token = localStorage.getItem('token');
+  const navigate = useNavigate();
 
-    const [history, setHistory] = useState<HistoryEntry[]>([]);
-    
-    useEffect(() => {
-        const load = async () => {
-            const presentation = await.getPresentationById(token!, Number(id));
-            setHistory(presentation.history || []); 
-        };
-        load();
-    }, []);
+  const [history, setHistory] = useState<HistoryEntry[]>([]);
+  
+  useEffect(() => {
+    const load = async () => {
+      const presentation = await getPresentationById(token!, Number(id));
+      if (!presentation) return;
+      setHistory(presentation.history || []); 
+    };
+    load();
+  }, []);
+
+  // used to restore curr history to curr presentation
+  const restore = async (entry: HistoryEntry) => {
+    const presentation = await getPresentationById(token!, Number(id));
+    const restored = {
+      ...entry.snapshot,
+      history: presentation?.history,
+    };
+
+    await updatePresentation(token!, restored);
+    navigate(`/presentation/${id}?slide=0`);
+  };
+
+  
 
 }
 export default HistoryPage;
