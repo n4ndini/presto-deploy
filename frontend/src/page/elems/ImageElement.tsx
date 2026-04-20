9c96514bf49914bdb42e1b718adbe09da63e7bc4
z5593045 <z5593045@ad.unsw.edu.au>
2026-04-20 16:21:19 +1000

    adding new resizing logic to image element

--- frontend/src/page/elems/ImageElement.tsx
+++ frontend/src/page/elems/ImageElement.tsx
@@ -1,5 +1,7 @@
-import type { MouseEvent } from "react";
+import type { CSSProperties, MouseEvent } from "react";
 import type { ImageElementType } from "../../types";
+
+type ResizeDirection = "n" | "s" | "e" | "w" | "ne" | "nw" | "se" | "sw";
 
 type Props = {
   elem: ImageElementType;
@@ -7,12 +9,14 @@ type Props = {
   onEdit: (elem: ImageElementType) => void;
   onSelect: () => void;
   onMoveStart: (e: MouseEvent, elem: ImageElementType) => void;
+  onResizeStart: (e: MouseEvent, elem: ImageElementType, direction: ResizeDirection) => void;
+  getResizeHandleStyle: (direction: ResizeDirection) => CSSProperties;
   isSelected: boolean;
 };
 
 // handles behaviour and appearance of Text Element
 // renders text box, handles right click delete, double click edit and styling
-function ImageElement({ elem, onDelete, onEdit, onSelect, onMoveStart, isSelected }: Props) {
+function ImageElement({ elem, onDelete, onEdit, onSelect, onMoveStart, onResizeStart, getResizeHandleStyle, isSelected }: Props) {
   return (
     <div
       onClick={(e) => {
@@ -47,6 +51,16 @@ function ImageElement({ elem, onDelete, onEdit, onSelect, onMoveStart, isSelecte
 
       }}
     >
+      {isSelected && (["nw", "n", "ne", "e", "se", "s", "sw", "w"] as ResizeDirection[]).map((direction) => (
+        <button
+          key={direction}
+          type="button"
+          aria-label={`Resize ${direction}`}
+          onMouseDown={(e) => onResizeStart(e, elem, direction)}
+          onClick={(e) => e.stopPropagation()}
+          style={getResizeHandleStyle(direction)}
+        />
+      ))}
       <img 
         src={elem.url}
         alt={elem.alt}
